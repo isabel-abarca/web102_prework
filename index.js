d27cea3f34a79f9b49666c7367eb4f8ef1279091
isabel-abarca <isabar404@gmail.com>
2026-05-02 09:33:33 -0500

    Completed Challenge #5

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function addGamesToPage(games) {
             <img src="${game.img}" class="game-img" />
             <h1>${game.name}</h1>
             <p>${game.description}</p>
+            <p>Raised Goal: ${game.goal}</p>
+            <p>Amount Raised: ${game.pledged}</p>
             <p>Backers: ${game.backers}</p>
         `;
 
@@ -103,10 +105,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-    
+    let listUnfundedGames = GAMES_JSON.filter( (game) => {
+        return game.pledged < game.goal
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(listUnfundedGames);
 }
 
 // show only games that are fully funded
@@ -114,10 +118,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let listFundedGames = GAMES_JSON.filter( (game) => {
+        return game.pledged >= game.goal
+    });
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(listFundedGames);
 }
 
 // show all games
@@ -125,13 +131,14 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
 // select each button in the "Our Games" section
-const unfundedBtn = document.getElementById("unfunded-btn");
-const fundedBtn = document.getElementById("funded-btn");
-const allBtn = document.getElementById("all-btn");
+const unfundedBtn = document.getElementById("unfunded-btn").addEventListener("click", filterUnfundedOnly);
+const fundedBtn = document.getElementById("funded-btn").addEventListener("click", filterFundedOnly);
+const allBtn = document.getElementById("all-btn").addEventListener("click", showAllGames);
 
 // add event listeners with the correct functions to each button
 
