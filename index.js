4133609ce24b6488b1b73946b1e6cad92767310a
isabel-abarca <isabar404@gmail.com>
2026-05-02 10:43:13 -0500

    Completed Challenge #6

--- index.js
+++ index.js
@@ -152,12 +152,18 @@ const allBtn = document.getElementById("all-btn").addEventListener("click", show
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const totalUnfunded = GAMES_JSON.reduce( (acc, game) => {
+    return acc + (game.pledged < game.goal);
+}, 0);
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayUnfunded = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for 7 games. Currently, 
+${totalUnfunded > 1 ? totalUnfunded : 1} games remain unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+unfundedDesc = document.createElement("p");
+unfundedDesc.innerHTML = displayUnfunded;
+descriptionContainer = appendChild(unfundedDesc);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
